ea51fc31e9c4595959e79abdfc2127d7f2f5cd02
Nikhil Kumar <nkumar70952-oss>
2026-09-18 23:34:52 +0530

    another class add

---
+++ src/day2/calculator.java
@@ -0,0 +1,26 @@
+package day2;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        int a,b,sum,sub,mul,div;
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your first value");
+        a = input.nextInt();
+
+        System.out.println("enter your second value");
+        b = input.nextInt();
+
+        sum =a+b;
+        sub= a-b;
+        mul = a*b;
+        div = a/b;
+
+        System.out.println("the sum is : " + sum);
+        System.out.println("the sub is : " + sub);
+        System.out.println("the multiplication is : " + mul);
+        System.out.println("the division is : " + div);
+
+    }
+}
